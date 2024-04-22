04faf5de160dddb33ad60d1a10725fd845bfe851
JorgeP3 <2588893300301@ingenieria.usac.edu.gt>
2024-04-22 15:12:38 -0600

    se levantaron los servidores

---
+++ Proyecto 3/frontend/myappdj/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views #va a al marchivo views y trae todo
+
+#http://localhost:8000/myappdj
+urlpatterns=[ 
+    path ("myform/", views.myform_view,name='myform'),#myform es la vista del navegador, no confundir con las peticiones del backend
+    path('get_response/',views.get_response_from_flask, name='get_response'),
+    path('get_response2/',views.get_response_from_flask2, name='get_response'),
+]
